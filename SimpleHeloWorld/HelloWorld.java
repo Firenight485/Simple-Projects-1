b45daf98619bc065c5aa3ba51f1b5b8d3e89bca9
Brian Bongermino <firenight485@gmail.com>
2021-09-16 22:54:58 -0400

    Hello word + name

---
+++ SimpleHeloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+package SimpleHeloWorld;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name;
+
+        System.out.println("Hello World! \nPlease tell me your name");
+        Scanner keyboard = new Scanner(System.in);
+        name = keyboard.nextLine();
+        
+        System.out.println("Nice to meet you "+name+"!");
+    }
+
+    
+
+}
