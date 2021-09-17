ba696b45da07680d9e8dbe880d9643dd6ac14066
Brian Bongermino <firenight485@gmail.com>
2021-09-17 03:16:06 -0400

    RPS 1

---
+++ RPS/RPS.java
@@ -0,0 +1,18 @@
+package RPS;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class RPS{
+    public static void main(String[] args) {
+        String player;
+        String computer;
+        int score;
+        
+        System.out.println("Welcome to Rock, Paper, Scissors! \n To win you need to beat the computer two times.");
+        System.out.println("Please enter either 'rock,' 'paper,' or 'scissors.'" );
+        Scanner in = new Scanner(System.in);
+    }
+}
+    
+
